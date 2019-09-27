012e184d56f513010c1aacc0107beb668b04f797
Bernhard F.W. Gschaider <bgschaid@hfd-research.com>
2019-09-27 23:34:48 +0200

    Added unittests for SolutionDirectory and found a minor bug/inconsistency

--- unittests/RunDictionary/test_SolutionDirectory.py
+++ unittests/RunDictionary/test_SolutionDirectory.py
@@ -12,6 +12,9 @@ from shutil import rmtree,copytree
 
 from .test_TimeDirectory import damBreakTutorial
 
+def chtMultiRegionTutorial():
+    prefix=foamTutorials()
+    return path.join(prefix,"heatTransfer","chtMultiRegionFoam","multiRegionHeater")
 
 class SolutionDirectoryTest(unittest.TestCase):
     def setUp(self):
@@ -53,5 +56,63 @@ class SolutionDirectoryTest(unittest.TestCase):
         del test[-1]
         self.assertEqual(len(test),0)
 
+@pytest.fixture
+def setupHeater(tmpdir,monkeypatch):
+    theDir=path.join(str(tmpdir),"heater")
+    copytree(chtMultiRegionTutorial(),theDir)
+    monkeypatch.chdir(theDir)
+    from subprocess import call
+    call("./Allrun.pre")
+
+@pytest.mark.skipif(foamTutorials()=='',reason="$FOAM_TUTORIALS is not defined")
+def test_SolutionDirectoryMultiRegionTest(setupHeater):
+    sol=SolutionDirectory(".")
+    assert sol.isValid()
+    assert len(sol.regions())==5
+    assert len(sol.getRegions())==5
+    assert "0" in sol
+    td=sol["0"]
+    assert type(td)==TimeDirectory
+    assert len(td)==7
+    assert sol.missingFiles()==[]
+    assert sol.nrProcs()==0
+    assert sol.getParallelTimes()==[]
+
+    assert path.basename(sol.latestDir())=="0"
+    assert path.basename(sol.initialDir())=="0"
+
+    assert sol.first=="0"
+
+    solTop=SolutionDirectory(".",region="topHeater")
+    assert not solTop.isValid()
+    solTop=SolutionDirectory(".",region="topAir")
+    assert solTop.isValid()
+    assert solTop.missingFiles()==[]
+    assert solTop.nrProcs()==0
+
+    assert sol.controlDict()==solTop.controlDict()
+    assert sol.systemDir()!=solTop.systemDir()
+
+    assert "0" in solTop
+    tdTop=solTop["0"]
+    assert type(tdTop)==TimeDirectory
+    assert len(tdTop)==7
+
+@pytest.fixture
+def setupHeaterParallel(setupHeater):
+    from subprocess import call
+    call(["decomposePar","-allRegions"])
+
+@pytest.mark.skipif(foamTutorials()=='',reason="$FOAM_TUTORIALS is not defined")
+def test_SolutionDirectoryMultiRegionParallelTest(setupHeaterParallel):
+    sol=SolutionDirectory(".",parallel=True)
+
+    assert sol.isValid()
+    assert sol.nrProcs()==4
+    assert sol.getParallelTimes()==["0"]
+
+    solTop=SolutionDirectory(".",region="topHeater")
+    assert solTop.nrProcs()==4
+    assert solTop.getParallelTimes()==["0"]
 
 # Should work with Python3 and Python2
